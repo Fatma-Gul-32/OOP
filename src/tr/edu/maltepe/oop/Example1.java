7f076421f2caba4e2e595ac7dc427dfb6863a4e4
FatmaGul <190704023@st.maltepe.edu.tr>
2022-03-01 13:44:08 +0300

    more lines added

--- src/tr/edu/maltepe/oop/Example1.java
+++ src/tr/edu/maltepe/oop/Example1.java
@@ -5,5 +5,8 @@ public class Example1 {
     public static void main(String[] args) {
 
         System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+
     }
 }
